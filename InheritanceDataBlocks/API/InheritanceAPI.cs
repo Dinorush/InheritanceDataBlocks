ffb144dfc1447a7f39eef3dce70ee955f7340c42
Dinorush <onparnorthstar@gmail.com>
2024-06-20 15:01:40 -0400

    Expose all root functions on API

--- InheritanceDataBlocks/API/InheritanceAPI.cs
+++ InheritanceDataBlocks/API/InheritanceAPI.cs
@@ -2,6 +2,7 @@
 using InheritanceDataBlocks.Inheritance;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace InheritanceDataBlocks.API
@@ -15,6 +16,9 @@ namespace InheritanceDataBlocks.API
         public static void AddDataBlock(T data, List<PropertyInfo> properties, uint parentID) => AddNode(data.persistentID, data, properties, parentID);
         public static bool AddNode(uint ID, T data, List<PropertyInfo> properties, uint parentID) => InheritanceResolver<T>.GetRoot().AddNode(ID, data, properties, parentID);
         public static bool AddNode(InheritanceNode<T> node) => InheritanceResolver<T>.GetRoot().AddNode(node);
+        public static bool RemoveNode(uint ID) => InheritanceResolver<T>.GetRoot().RemoveNode(ID);
+        public static InheritanceNode<T>? GetNode(uint ID) => InheritanceResolver<T>.GetRoot().GetNode(ID);
+        public static bool TryGetNode(uint ID, [MaybeNullWhen(false)] out InheritanceNode<T> node) => InheritanceResolver<T>.GetRoot().TryGetNode(ID, out node);
         public static LinkedList<InheritanceNode<T>>? GetInheritanceList(uint ID) => InheritanceResolver<T>.GetRoot().GetInheritanceList(ID);
         public static void DebugPrintAllPaths(string? groupName = null) => InheritanceResolver<T>.GetRoot().DebugPrintAllPaths(groupName);
     }
