17496a9ed54c51d6d156d8a9593502402a0dfb5d
Dinorush <onparnorthstar@gmail.com>
2024-07-01 21:56:29 -0400

    Version bump

--- InheritanceDataBlocks/EntryPoint.cs
+++ InheritanceDataBlocks/EntryPoint.cs
@@ -5,7 +5,7 @@ using InheritanceDataBlocks.Utils;
 
 namespace InheritanceDataBlocks;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.1.3")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.1.4")]
 [BepInDependency("GTFO.InjectLib", BepInDependency.DependencyFlags.HardDependency)]
 internal sealed class EntryPoint : BasePlugin
 {
